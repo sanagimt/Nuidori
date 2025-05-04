f3e3c4b7f26225fb0f52a51ae45b3b25eb69de98
sanagimt <180413622+sanagimt@users.noreply.github.com>
2025-05-04 07:24:47 +0000

    devise（public,admin)導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  devise_for :users, controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  devise_for :admins, controllers: {
+    sessions: "admin/sessions"
+  }
+
   root to: 'homes#top'
 
 end
