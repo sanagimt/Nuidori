8f3ccade5b4a41acd1e22664e7a2a3192d6c5798
sanagimt <180413622+sanagimt@users.noreply.github.com>
2025-05-19 21:21:08 +0000

    いいね機能追加（非同期実装）

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import "jquery";
+import $ from "jquery"
+window.$ = $
+window.jQuery = $
+
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"; 
