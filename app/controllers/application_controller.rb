6082cce24feb0bcf668f856dc61ebe833f195f1d
sanagimt <180413622+sanagimt@users.noreply.github.com>
2025-06-13 07:23:45 +0000

    application controller修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::Base
 
   def authenticate_admin!
-    unless admin_signed_in?
+    return if admin_signed_in?
       redirect_to root_path, alert: '指定したURLのアクセス権限がありません。'
-    end
   end
 
 end
